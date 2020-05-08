6865169959dacf20d5205fc2f5f928923b0c4297
Pankaj Khedekar <pkkhedekar@gmail.com>
2020-05-08 14:11:28 +0530

    Micronaut data implementation

--- employee-service/src/main/java/com/example/model/Employee.java
+++ employee-service/src/main/java/com/example/model/Employee.java
@@ -1,13 +1,26 @@
 package com.example.model;
 
+import io.micronaut.data.annotation.Id;
+import io.micronaut.data.annotation.MappedEntity;
+import io.micronaut.data.model.naming.NamingStrategies;
+
+@MappedEntity(namingStrategy = NamingStrategies.LowerCase.class)
 public class Employee {
 
+    @Id
     private Long id;
     private Long organizationId;
     private Long departmentId;
     private String name;
     private int age;
-    private String position;
+
+    public Employee(Long id, Long organizationId, Long departmentId, String name, int age) {
+        this.id = id;
+        this.organizationId = organizationId;
+        this.departmentId = departmentId;
+        this.name = name;
+        this.age = age;
+    }
 
     public Employee() {
 
@@ -53,17 +66,13 @@ public class Employee {
         this.age = age;
     }
 
-    public String getPosition() {
-        return position;
-    }
-
-    public void setPosition(String position) {
-        this.position = position;
-    }
-
-    @Override
-    public String toString() {
-        return "Employee [id=" + id + ", organizationId=" + organizationId + ", departmentId=" + departmentId
-                + ", name=" + name + ", position=" + position + "]";
+    @Override public String toString() {
+        return "Employee{" +
+            "id=" + id +
+            ", organizationId=" + organizationId +
+            ", departmentId=" + departmentId +
+            ", name='" + name + '\'' +
+            ", age=" + age +
+            '}';
     }
 }
