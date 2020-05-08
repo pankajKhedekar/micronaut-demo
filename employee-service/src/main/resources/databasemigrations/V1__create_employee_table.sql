c4bd4244945fad78a870b73e0a641ed0bf935fe3
Pankaj Khedekar <pkkhedekar@gmail.com>
2020-05-08 11:38:41 +0530

    flyway config added

---
+++ employee-service/src/main/resources/databasemigrations/V1__create_employee_table.sql
@@ -0,0 +1,9 @@
+--create table employee(
+--     id SERIAL PRIMARY KEY,
+--     organizationId integer,
+--     departmentId integer,
+--     name varchar(40),
+--     age integer
+--)
+
+create database testMigration;
